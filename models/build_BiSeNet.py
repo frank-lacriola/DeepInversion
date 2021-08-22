4c49853f4e60e7b273df500f82508699178c8644
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-08-22 18:14:17 +0200

    Fixed typo

--- models/build_BiSeNet.py
+++ models/build_BiSeNet.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from modules.build_contextpath import build_contextpath
+from models.build_contextpath import build_contextpath
 import warnings
 warnings.filterwarnings(action='ignore')
 
